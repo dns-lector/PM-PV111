f439e071c96f67f104e88baf9bee70575bccaa5b
DenNikSam <denniksam@gmail.com>
2024-01-19 19:03:25 +0200

    9

--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!(7)");
 Console.WriteLine("Hello, World!(5)");
+
+Console.WriteLine("Hello, World!(9)");
